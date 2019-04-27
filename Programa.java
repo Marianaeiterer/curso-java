18ee87922107adb329c01e17b7dc98c8c30ac439
Mariana Lopes <marieiterer@gmail.com>
2019-04-26 22:35:57 -0300

    Persistence

--- Programa.java
+++ Programa.java
@@ -1,8 +1,6 @@
 package aplicacao;
 
 import dominio.Pessoa;
-import dominio.Policial;
-
 public class Programa {
 
 	public static void main(String[] args) {
@@ -10,15 +8,11 @@ public class Programa {
 		Pessoa p1= new Pessoa (1,"Carlos da Silva", "carlos@gmail.com");
 		Pessoa p2= new Pessoa (2, "Joaquim torres", "joaquim@gmail.com");
 		Pessoa p3= new Pessoa (3, "Mariana lopes", "mariana@gmail.com");
-		Policial p4= new Policial (4, "manoel", "manoel@gmail.com", "4m");
 		System.out.println(p1);
 		System.out.println(p2);
 		System.out.println(p3);
-		System.out.println(p4.getDele());
-		p4.setDele("12");
-		System.out.println(p4.getDele());
-	
 	
 	}
 
 }
+ 
