8c9e4b764288a15261720e50dfa0bc18539c2e45
Mariana Lopes <marieiterer@gmail.com>
2019-04-29 16:12:33 -0300

    Salvando no xamp

--- Programa.java
+++ Programa.java
@@ -1,16 +1,27 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1= new Pessoa (1,"Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2= new Pessoa (2, "Joaquim torres", "joaquim@gmail.com");
-		Pessoa p3= new Pessoa (3, "Mariana lopes", "mariana@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p1= new Pessoa (null,"Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2= new Pessoa (null, "Joaquim torres", "joaquim@gmail.com");
+		Pessoa p3= new Pessoa (null, "Mariana lopes", "mariana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName)
+		EntityManager em = emf.createEntityManager()
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        
+        System.out.println("Pronto!");
 	
 	}
 
--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,24 +1,29 @@
 package aplicacao;
 
-import dominio.Pessoa;
-import dominio.Policial;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1= new Pessoa (1,"Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2= new Pessoa (2, "Joaquim torres", "joaquim@gmail.com");
-		Pessoa p3= new Pessoa (3, "Mariana lopes", "mariana@gmail.com");
-		Policial p4= new Policial (4, "manoel", "manoel@gmail.com", "4m");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-		System.out.println(p4.getDele());
-		p4.setDele("12");
-		System.out.println(p4.getDele());
-	
+		Pessoa p1= new Pessoa (null,"Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2= new Pessoa (null, "Joaquim torres", "joaquim@gmail.com");
+		Pessoa p3= new Pessoa (null, "Mariana lopes", "mariana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        
+        System.out.println("Pronto!");
 	
 	}
 
 }
+
