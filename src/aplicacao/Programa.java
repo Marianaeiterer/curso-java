5fadb0558eb319d160150514e8c76b6818ea97ac
Mariana Lopes <marieiterer@gmail.com>
2019-04-29 16:19:56 -0300

    buscando um objeto

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -9,19 +9,18 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1= new Pessoa (null,"Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2= new Pessoa (null, "Joaquim torres", "joaquim@gmail.com");
-		Pessoa p3= new Pessoa (null, "Mariana lopes", "mariana@gmail.com");
-		
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
         
         System.out.println("Pronto!");
+        em.close();
+        emf.close();
+        
 	
 	}
 
